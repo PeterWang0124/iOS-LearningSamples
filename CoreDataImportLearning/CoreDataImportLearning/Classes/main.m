407227c2ac45aa95e341ed7773c75c0db47680eb
Peter Wang <peter0124.wang@wayi.com.tw>
2014-12-31 09:57:28 +0800

    Create core data import learning project.

---
+++ CoreDataImportLearning/CoreDataImportLearning/Classes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoreDataImportLearning
+//
+//  Created by PeterWang on 12/31/14.
+//  Copyright (c) 2014 PeterWang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
