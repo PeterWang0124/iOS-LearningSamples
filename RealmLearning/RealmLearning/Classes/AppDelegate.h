352b96905c23f5c5589d11a364592d53cd9060d8
Peter Wang <peter0124.wang@wayi.com.tw>
2015-01-05 11:25:32 +0800

    Create Realm learning project.

---
+++ RealmLearning/RealmLearning/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmLearning
+//
+//  Created by PeterWang on 1/5/15.
+//  Copyright (c) 2015 PeterWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealmLearning/RealmLearning/Classes/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealmLearning
+//
+//  Created by PeterWang on 1/5/15.
+//  Copyright (c) 2015 PeterWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
