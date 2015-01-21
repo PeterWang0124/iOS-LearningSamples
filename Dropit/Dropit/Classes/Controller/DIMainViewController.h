d0f925bc4e6fa30f78c5343d6f29f352675cc95c
Peter Wang <peter0124.wang@wayi.com.tw>
2015-01-21 13:43:37 +0800

    Create Dropit Porject for Stanford CS193p Lecture 8 to practice about blocks and UI animations.

---
+++ Dropit/Dropit/Classes/Controller/DIMainViewController.h
@@ -0,0 +1,15 @@
+//
+//  DIMainViewController.h
+//  Dropit
+//
+//  Created by PeterWang on 1/21/15.
+//  Copyright (c) 2015 PeterWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DIMainViewController : UIViewController
+
+
+@end
+
---
+++ Dropit/Dropit/Classes/Controller/DIMainViewController.m
@@ -0,0 +1,27 @@
+//
+//  DIMainViewController.m
+//  Dropit
+//
+//  Created by PeterWang on 1/21/15.
+//  Copyright (c) 2015 PeterWang. All rights reserved.
+//
+
+#import "DIMainViewController.h"
+
+@interface DIMainViewController ()
+
+@end
+
+@implementation DIMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
