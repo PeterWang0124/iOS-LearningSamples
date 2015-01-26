6645aece2eb96f7d0911e7736689d37eb21e0ea0
Peter Wang <peter0124.wang@wayi.com.tw>
2015-01-26 17:22:49 +0800

    Create Imaginarium project.

---
+++ Imaginarium/Imaginarium/Classes/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imaginarium
+//
+//  Created by PeterWang on 1/26/15.
+//  Copyright (c) 2015 PeterWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Imaginarium/Imaginarium/Classes/Controller/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Imaginarium
+//
+//  Created by PeterWang on 1/26/15.
+//  Copyright (c) 2015 PeterWang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
