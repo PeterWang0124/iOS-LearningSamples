2acfca3ca03a75897c3fe056271e562d5fdfc213
Peter Wang <peter0124.wang@wayi.com.tw>
2015-02-11 14:58:08 +0800

    Modify MySIPPhone about init pjsua.

---
+++ MySIPPhone/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,22 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// OpenSSL-Universal
+#define COCOAPODS_POD_AVAILABLE_OpenSSL_Universal
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.0.1.k.
+
+// pjsip
+#define COCOAPODS_POD_AVAILABLE_pjsip
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 2.3.0.7.
+
